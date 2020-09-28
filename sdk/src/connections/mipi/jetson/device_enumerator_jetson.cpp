e0baa17d9e7e1d7f9a49350719f4dac47e8ddafb
Virgil Litan <virgil.litan@analog.com>
2020-09-28 19:32:33 +0300

    copy raspberry implementation
    Signed-off-by: Virgil Litan <virgil.litan@analog.com>

--- sdk/src/connections/mipi/jetson/device_enumerator_jetson.cpp
+++ sdk/src/connections/mipi/jetson/device_enumerator_jetson.cpp
@@ -41,6 +41,112 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+namespace local {
+
+aditof::Status findDevicePathsAtMedia(const std::string &media,
+                                      std::string &dev_name,
+                                      std::string &subdev_name) {
+    using namespace aditof;
+    using namespace std;
+
+    char *buf;
+    int size = 0;
+    int idx = 0;
+
+    /* Run media-ctl to get the video processing pipes */
+    char cmd[64];
+    sprintf(cmd, "sudo media-ctl -d %s -p", media.c_str());
+    FILE *fp = popen(cmd, "r");
+    if (!fp) {
+        LOG(WARNING) << "Error running media-ctl";
+        return Status::GENERIC_ERROR;
+    }
+
+    /* Read the media-ctl output stream */
+    buf = (char *)malloc(128 * 1024);
+    while (!feof(fp)) {
+        fread(&buf[size], 1, 1, fp);
+        size++;
+    }
+    pclose(fp);
+    buf[size] = '\0';
+
+    /* Get the list of entities from output buffer */
+    string str(buf);
+    free(buf);
+    vector<string> entities;
+    size_t pos = str.find("entity");
+    while (pos != string::npos) {
+        size_t pos1 = str.find("entity", pos + 1);
+        if (pos1 != string::npos) {
+            string str1(str, pos, pos1 - pos);
+            entities.push_back(str1);
+        } else {
+            string str1(str, pos, size - pos);
+            entities.push_back(str1);
+            break;
+        }
+        pos = pos1;
+    }
+
+    /* Find the names of the associated device and subdevice */
+    string fstr("<- \"addi903x");
+    for (idx = 0; idx < entities.size(); idx++) {
+        size_t found = entities[idx].find(fstr);
+        size_t found1 = 0;
+        if (found != string::npos) {
+            found = entities[idx].find("Source", found);
+            if (found == string::npos) {
+                found = entities[idx].find("device node name ");
+                if (found == string::npos) {
+                    break;
+                }
+                found1 = entities[idx].find("pad", found);
+                if (found1 == string::npos) {
+                    break;
+                }
+                dev_name = entities[idx].substr(
+                    found + strlen("device node name "),
+                    found1 - found - strlen("device node name ") - 2);
+                break;
+            }
+            found = entities[idx].find("ENABLED", found);
+            if (found != string::npos) {
+                found = entities[idx].rfind("\"", found);
+                found1 = entities[idx].rfind("\"", found - 1);
+                string str1(entities[idx], found1, found - found1);
+                fstr = "<- " + str1;
+                continue;
+            }
+        }
+    }
+    fstr = "addi903x";
+    for (; idx < entities.size(); idx++) {
+        size_t found = entities[idx].find(fstr);
+        size_t found1 = 0;
+        if (found == string::npos) {
+            continue;
+        }
+
+        found = entities[idx].find("device node name ");
+        if (found == string::npos) {
+            break;
+        }
+        found1 = entities[idx].find("pad", found);
+        if (found1 == string::npos) {
+            break;
+        }
+        subdev_name = entities[idx].substr(found + strlen("device node name "),
+                                           found1 - found -
+                                               strlen("device node name ") - 2);
+        break;
+    }
+
+    return Status::OK;
+}
+
+}; // namespace local
+
 aditof::Status DeviceEnumeratorImpl::findDevices(
     std::vector<aditof::DeviceConstructionData> &devices) {
     using namespace aditof;
@@ -65,31 +171,55 @@ aditof::Status DeviceEnumeratorImpl::findDevices(
     }
     closedir(dirp);
 
-    for (const auto &video : videoPaths) {
-        std::string devPath;
+    for (const auto &media : videoPaths) {
+        DLOG(INFO) << "Looking at: " << media << " for an eligible TOF camera";
 
-        if (devPath.empty()) {
+        std::string devPath;
+        std::string subdevPath;
+
+        status = local::findDevicePathsAtMedia(media, devPath, subdevPath);
+        if (status != Status::OK) {
+            LOG(WARNING) << "failed to find device paths at media: " << media;
+            return status;
+        }
+
+        if (devPath.empty() || subdevPath.empty()) {
             continue;
         }
+
+        DeviceConstructionData devData;
+        devData.connectionType = ConnectionType::LOCAL;
+        devData.driverPath = devPath + ";" + subdevPath;
+
+        // Check if EEPROM is available
+        struct stat st;
+        if (stat(EEPROM_DEV_PATH, &st) == 0) {
+            EepromConstructionData eData;
+            eData.driverName = EEPROM_NAME;
+            eData.driverPath = EEPROM_DEV_PATH;
+            devData.eeproms.emplace_back(eData);
+        }
+
+        devices.emplace_back(devData);
     }
-    // TO DO: Don't guess the device, find a way to identify it so we are sure
-    // we've got the right device and is compatible with the SDK
-    DeviceConstructionData devData;
-    devData.connectionType = ConnectionType::LOCAL;
-    devData.driverPath = "/dev/video0";
+    // // TO DO: Don't guess the device, find a way to identify it so we are sure
+    // // we've got the right device and is compatible with the SDK
+    // DeviceConstructionData devData;
+    // devData.connectionType = ConnectionType::LOCAL;
+    // devData.driverPath = "/dev/video0";
 
-    struct stat st;
-    if (stat(EEPROM_DEV_PATH, &st) == 0) {
-        EepromConstructionData eData;
-        eData.driverName = EEPROM_NAME;
-        eData.driverPath = EEPROM_DEV_PATH;
-        devData.eeproms.emplace_back(eData);
-    }
+    // struct stat st;
+    // if (stat(EEPROM_DEV_PATH, &st) == 0) {
+    //     EepromConstructionData eData;
+    //     eData.driverName = EEPROM_NAME;
+    //     eData.driverPath = EEPROM_DEV_PATH;
+    //     devData.eeproms.emplace_back(eData);
+    // }
 
-    devices.emplace_back(devData);
+    // devices.emplace_back(devData);
 
-    DLOG(INFO) << "Looking at: " << devData.driverPath
-               << " for an eligible TOF camera";
+    // DLOG(INFO) << "Looking at: " << devData.driverPath
+    //            << " for an eligible TOF camera";
 
     return status;
 }
